ecb1a07232a02ee10d3e9bf50b4b40f0f5c4d7f9
andry <andry@DESKTOP-SENM7HG>
2020-10-13 09:18:53 +0200

    Prima versione dell'esercizio che effettua la conversione da decimale a binario.

---
+++ ConversioniDecimali_Binario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConversioniDecimali_Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
